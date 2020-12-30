a1a20efaa70e1eea30ae0e8f241bb7172bdb1644
Yuriy work-pc <transpmailbox@gmail.com>
2020-12-30 12:36:36 +0200

    add function reduce to categories in array utils

--- src/plugins/arrayUtils.js
+++ src/plugins/arrayUtils.js
@@ -13,3 +13,32 @@ export function deleteIfExist(array, item, prop = 'id') {
         array.splice(index, 1);
     }
 }
+
+export function reduceToCategories(array,objProp){
+    return array.reduce((result,value) =>{
+        if(!containCategory(result,value[objProp])){
+            result.push({category:value[objProp], items:[]});
+        }
+        putToCategory(result,value,objProp)
+        return result;
+    },[]);
+}
+
+const containCategory = (array,category) => {
+    if(typeof category === 'object' && category.id !== undefined){
+        return array.findIndex(element => element.category.id === category.id) !==-1;
+    }
+    return array.findIndex(element => element.category === category) !==-1;
+}
+
+const putToCategory = (array,obj,objProp) =>{
+    let index = -1;
+    if(typeof obj[objProp] === 'object' && obj[objProp].id !== undefined){
+        index= array.findIndex(item => item.category.id === obj[objProp].id);
+    }else{
+        index= array.findIndex(item => item.category === obj[objProp]);
+    }
+    if(index !== -1){
+        array[index].items.push(obj);
+    }
+}
